f0e153c050ccc23096b4f1e2035d2c67ab0ef098
markipoli <mark.trubiani@gmail.com>
2026-08-10 08:56:47 +1000

    arrow scaling

--- ball.gd
+++ ball.gd
@@ -11,6 +11,8 @@ var drag_vector := Vector2.ZERO
 var start_mouse_pos := Vector2.ZERO
 var angle_speed := 90.0    # degrees per second
 var power_speed := 50.0    # units per second
+var min_distance = 0.08
+var max_distance = 0.3
 @export var debug_rays: bool = false
 @onready var camera: Camera3D = %Camera3D
 var original_offset: Vector3
@@ -108,11 +110,12 @@ func _physics_process(delta):
 			)
 
 
-	print("Mouse: ", mouse_pos)
-	print("Ray origin: ", ray_origin)
-	print("Ray direction: ", ray_direction)
-	print("Mouse world: ", mouse_world_pos)
-	print("Ball: ", global_position)
+	#print("Mouse: ", mouse_pos)
+	#print("Ray origin: ", ray_origin)
+	#print("Ray direction: ", ray_direction)
+	#print("Mouse world: ", mouse_world_pos)
+	#print("Ball: ", global_position)
+	
 	%Camera3D.global_transform.origin = global_transform.origin + original_offset
 	## --- AIM LEFT/RIGHT ---
 	#if Input.is_action_pressed("ui_left"):
@@ -131,39 +134,56 @@ func _physics_process(delta):
 		#sin(deg_to_rad(yaw)),
 		#cos(deg_to_rad(yaw))
 	#)
-	if mouse_world_pos != null:
-		var direction = global_position - mouse_world_pos
-		direction.y = 0
-
-		if direction.length_squared() > 0.001:
-			direction = direction.normalized()
-
-			# This is now the ball's aim direction
-			angle = Vector2(direction.x, direction.z)
+	#if mouse_world_pos != null:
+		##print("Distance: ", global_position.distance_to(mouse_world_pos))
+		#var direction = global_position - mouse_world_pos
+		#direction.y = 0
+#
+		#if direction.length_squared() > 0.001:
+			#direction = direction.normalized()
+#
+			## This is now the ball's aim direction
+			#angle = Vector2(direction.x, direction.z)
 
 	%ShotArrow.rotation.y = atan2(angle.x, angle.y) + PI
 	%ShotArrow.rotation.z = 0
 	%ShotArrow.rotation.x = 0
 	%ShotArrow.position = position
-	# --- POWER CONTROL ---
-	if Input.is_action_just_pressed("ui_up"):
-		power += power_speed * delta
-		print("Power: ", power)
-	if Input.is_action_just_pressed("ui_down"):
-		power -= power_speed * delta
-		print("Power: ", power)
-
-	power = clamp(power, min_power, max_power)
+	## --- POWER CONTROL ---
+	#if Input.is_action_just_pressed("ui_up"):
+		#power += power_speed * delta
+		#print("Power: ", power)
+	#if Input.is_action_just_pressed("ui_down"):
+		#power -= power_speed * delta
+		#print("Power: ", power)
+#
+	#power = clamp(power, min_power, max_power)
 
 	# --- SHOOT ---
-	if Input.is_action_just_pressed("shoot"):
-		print("SHOT!")
-		print("Yaw (deg): ", yaw)
-		print("Power: ", power)
+	if mouse_world_pos !=null:
+		var shot_direction = global_position - mouse_world_pos
+		shot_direction.y = 0
+		if shot_direction.length_squared() > 0.001:
+			shot_direction = shot_direction.normalized()
+			angle = Vector2(shot_direction.x, shot_direction.z)
+		var distance  = global_position.distance_to(mouse_world_pos)
+		#print("Unclamped distance" + str(distance))
+		distance = clamp(distance,min_distance,max_distance)
+		#print("Clamped distance" + str(distance))
+		var percent_power_meter = (distance - min_distance) / (max_distance - min_distance)
+		#print("Percent power meter" + str(percent_power_meter))
+		var clamped_power_meter = clamp(percent_power_meter, 0.2, 1)
+		%ShotArrow.scale = Vector3(clamped_power_meter,clamped_power_meter,clamped_power_meter)
+		var power: float = clamped_power_meter * 5
+		#print("Power: ", str(power))
+		
+		if Input.is_action_just_pressed("shoot"):
+			print("SHOT!")
+			print("Power: ", power)
 
-		# Apply force in the aimed direction
-		var force := Vector3(angle.x, 0, angle.y) * power
-		apply_central_impulse(force)
+			# Apply force in the aimed direction
+			var force := Vector3(angle.x, 0, angle.y) * power
+			apply_central_impulse(force)
 
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
